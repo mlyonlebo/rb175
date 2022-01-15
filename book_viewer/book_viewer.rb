1d52de60f6d9e28940811f59db3e5a175a669a09
Max Lebo <mlebo@Maxs-MacBook-Air.local>
2022-01-15 08:58:54 -0600

    updating book viewer project

--- book_viewer/book_viewer.rb
+++ book_viewer/book_viewer.rb
@@ -2,17 +2,63 @@ require "tilt/erubis"
 require "sinatra"
 require "sinatra/reloader"
 
+before do
+  @contents = File.readlines("data/toc.txt")
+end
+
+helpers do
+
+  def in_numbered_paragraphs(chapter)
+    id_count = 0
+    chapter.split("\n\n").map do |graf|
+      id_count += 1
+      "<p id='#{id_count.to_s}'>#{graf}</p>"
+    end.join
+  end
+
+  def bold_match(graf, query)
+    strong_query = "<strong>#{query}</strong>"
+    graf.gsub(query, strong_query)
+  end
+
+  def search_results(chapters, query)
+    results = Hash.new([])
+    chapters.each_with_index do |chapter, ch_idx|
+      File.read(chapter).split("\n\n").each_with_index do |graf, p_idx|
+        if graf.include?(query)
+          graf = bold_match(graf, query)
+          results[@contents[ch_idx]] += ["<li><a href=" + "/chapters/#{ch_idx+1}##{p_idx+1}" + ">#{graf}</a></li>"]
+        end
+      end
+    end
+    results
+  end
+end
+
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
-  @chapters = File.readlines("data/toc.txt")
+
   erb :home
 end
 
-get "/chapters/1" do
-  @title = "Chapter 1"
-  @chapters = File.readlines("data/toc.txt")
-  @chapter_one = File.read("data/chp1.txt")
-  @paragraphs = @chapter_one.split("\n\n")  
+get "/chapters/:number" do
+  @chap_num = params['number']
+  @title = @contents[@chap_num.to_i - 1]
+  redirect "/" unless (1..@contents.size).cover?(@chap_num.to_i)
+
+  @chapter = File.read("data/chp#{@chap_num}.txt")
   
-  erb :chapter_one
+  erb :chapter
+
+end
+
+get "/search" do
+  @query = params[:query]
+  @chapters = Dir.glob("data/*")
+
+  erb :search
+end
+
+not_found do
+  redirect "/"
 end
